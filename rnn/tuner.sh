220ec49ec32306a224d88c1cbd524cb1c5379c5b
NickTheWhale <loehrketurkey7@gmail.com>
2024-10-18 03:22:18 -0500

    60%

--- rnn/tuner.sh
+++ rnn/tuner.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-hidden_sizes=(32 64 128)
+hidden_sizes=(32 64 128 256)
 layers=(1 2)
 batch_sizes=(64 128 256)
 learning_rates=(0.0001 0.0005 0.001)
-epochs=(2 4)
+epochs=(10 50 250)
 folds=(3 5)
 
-# Loop through all combinations of parameters
 for hidden_size in "${hidden_sizes[@]}"; do
     for layer in "${layers[@]}"; do
         for batch_size in "${batch_sizes[@]}"; do
             for learning_rate in "${learning_rates[@]}"; do
                 for epoch in "${epochs[@]}"; do
                     for fold in "${folds[@]}"; do
-                        # Construct the command
                         command="python rnn.py data/embeddings_rall_p500.pkl --hidden_size $hidden_size --layers $layer --batch_size $batch_size --learning_rate $learning_rate --epochs $epoch --folds $fold"
                         
-                        # Execute the command and capture the output
                         echo "Running: $command"
                         output=$(eval $command)
                         echo "$output"
