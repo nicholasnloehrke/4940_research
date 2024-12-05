3f02d15ffb6c1c821f76c2d0b459bc44bc48b947
nicholasnloehrke <loehrketurkey7@gmail.com>
2024-12-05 00:33:59 -0600

    added pipeline for texas

--- pipeline_yes_augmentation.sh
+++ pipeline_yes_augmentation.sh
@@ -5,7 +5,8 @@
 #####################################################################################
 
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-OUTPUT_DIR="runs/$TIMESTAMP"
+# OUTPUT_DIR="runs/$TIMESTAMP"
+OUTPUT_DIR="runs/yes_augmentation-$TIMESTAMP"
 INPUT_REVIEWS="data/raw_reviews.csv"
 # INPUT_REVIEWS="data/raw_reviews_small.csv"
 HIDDEN_STATE_EMBEDDING_SIZE=$(python -c "from config.config import config; print(config.ffnn.hidden_state_embedding_size)")
@@ -18,8 +19,8 @@ cp config/config.py $OUTPUT_DIR/config.py
 python -m scripts.create_train_test_datasets $INPUT_REVIEWS --train_outfile $OUTPUT_DIR/raw_training_reviews.csv --test_outfile $OUTPUT_DIR/raw_test_reviews.csv --test_size 0.15
 
 # Preprocess
-# python -m scripts.preprocess_reviews $OUTPUT_DIR/raw_training_reviews.csv -o $OUTPUT_DIR/preprocessed_training_reviews.csv --chunk 10 --max_chunks 100 --shuffle_chunk 30
-python -m scripts.preprocess_reviews $OUTPUT_DIR/raw_training_reviews.csv -o $OUTPUT_DIR/preprocessed_training_reviews.csv 
+python -m scripts.preprocess_reviews $OUTPUT_DIR/raw_training_reviews.csv -o $OUTPUT_DIR/preprocessed_training_reviews.csv --chunk 10 --max_chunks 100 --shuffle_chunk 30
+# python -m scripts.preprocess_reviews $OUTPUT_DIR/raw_training_reviews.csv -o $OUTPUT_DIR/preprocessed_training_reviews.csv 
 
 # Generate training embeddings
 python -m scripts.generate_embeddings fasttext/crawl-300d-2M-subword.bin $OUTPUT_DIR/preprocessed_training_reviews.csv -o $OUTPUT_DIR/training_embeddings.pkl
@@ -41,10 +42,10 @@ python -m scripts.train_ffnn $OUTPUT_DIR/training_hidden_states.pkl -o $OUTPUT_D
 python -m scripts.generate_embeddings fasttext/crawl-300d-2M-subword.bin $OUTPUT_DIR/raw_test_reviews.csv -o $OUTPUT_DIR/test_embeddings.pkl
 
 # Evaluate RNN
-python -m scripts.evaluate_rnn $OUTPUT_DIR/rnn.model $OUTPUT_DIR/test_embeddings.pkl
+python -m scripts.evaluate_rnn $OUTPUT_DIR/rnn.model $OUTPUT_DIR/test_embeddings.pkl | tee $OUTPUT_DIR/rnn_eval.txt
 
 # Extract hidden states
 python -m scripts.extract_hidden_states $OUTPUT_DIR/rnn.model $OUTPUT_DIR/test_embeddings.pkl -d $HIDDEN_STATE_EMBEDDING_SIZE -o $OUTPUT_DIR/test_hidden_states.pkl
 
 # Evaluate FFNN
-python -m scripts.evaluate_ffnn $OUTPUT_DIR/ffnn.model $OUTPUT_DIR/test_hidden_states.pkl
+python -m scripts.evaluate_ffnn $OUTPUT_DIR/ffnn.model $OUTPUT_DIR/test_hidden_states.pkl | tee $OUTPUT_DIR/ffnn_eval.txt
